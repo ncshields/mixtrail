3820efb841c161d3694f26c35c78686b3c4b059e
ncshields <44266835+ncshields@users.noreply.github.com>
2025-10-26 22:55:55 -0400

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-import Planner from "@/components/Planner";
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const Planner = dynamic(async () => {
+  const mod = await import('@/components/Planner');
+  return (mod as any).Planner ?? (mod as any).default;
+}, { ssr: false });
 
 export default function Page() {
   return (
